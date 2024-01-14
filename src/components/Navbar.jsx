fb07c79c1b428e87fc59582ba709bf1414bc505e
Atif Hameed <atifhameed11312@gmail.com>
2024-01-14 21:25:27 +0500

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,18 +6,31 @@ const Navbar = () => {
 
     const items = useSelector( (state) => state.cart )
 
+    const navItems = [
+        {label : 'Home', url : '/'},
+        {label : 'Cart', url : '/cart'}
+    ]
+
     return (
         <>
             <div className='flex items-center flex-wrap md:px-16 px-4'>
                 <div className='md:w-[35%] text-4xl font-bold'>Redux Store</div>
                 <div className='md:w-[65%] flex items-center justify-between'>
                     <div className='flex md:w-1/2 justify-evenly gap-3 items-center'>
-                        <Link to='/' className='text-xl font-medium text-blue-800'>
+                        {
+                            navItems.map( (item ,index) => (
+                                <Link to={item.url} className='text-xl font-medium text-blue-800' >
+                                    {item.label}
+                                </Link>
+                            ) )
+                        }
+
+                        {/* <Link to='/' className='text-xl font-medium text-blue-800'>
                             Home
                         </Link>
                         <Link to='/cart' className='text-xl font-medium text-blue-800'>
                             Cart
-                        </Link>
+                        </Link> */}
                     </div>
                     <div className='text-xl font-bold text-green-700'>Cart items : <span className='text-black' >{items.length}</span></div>
                 </div>
